9c403b72e66643a8f9e8d314648be8fb0dc129f9
capitanfuturo <giuseppe.caliendo@gmail.com>
2017-12-07 21:38:41 +0100

    Stock fix

--- app/report/report.controller.js
+++ app/report/report.controller.js
@@ -25,34 +25,36 @@ controller.getMonthlySales = function (req, res) {
         var result = new Map();
         for (var i = 0; i < size; i++) {
           var row = data[i];
-          var key = row.item._id;
-          if (row.warehouse) {
-            key = row.warehouse._id + '|' + row.item._id;
-          }
-
-          if (result.has(key)) {
-            var value = result.get(key);
-            value.quantity = value.quantity + row.quantity;
-            value.price = value.price + row.price;
-            result.set(key, value);
-          } else {
-            var warehouse = '';
+          if (row.item && row.item._id && row.warehouse && row.warehouse._id) {
+            var key = row.item._id;
             if (row.warehouse) {
-              warehouse = row.warehouse.name;
+              key = row.warehouse._id + '|' + row.item._id;
             }
-            var quantity = row.quantity;
-            var price = row.price;
 
-            var newValue = {
-              "warehouse": warehouse,
-              "model": row.item.model,
-              "category": row.item.category,
-              "gender": row.item.gender,
-              "size": row.item.size,
-              "quantity": quantity,
-              "price": price
-            };
-            result.set(key, newValue);
+            if (result.has(key)) {
+              var value = result.get(key);
+              value.quantity = value.quantity + row.quantity;
+              value.price = value.price + row.price;
+              result.set(key, value);
+            } else {
+              var warehouse = '';
+              if (row.warehouse) {
+                warehouse = row.warehouse.name;
+              }
+              var quantity = row.quantity;
+              var price = row.price;
+
+              var newValue = {
+                "warehouse": warehouse,
+                "model": row.item.model,
+                "category": row.item.category,
+                "gender": row.item.gender,
+                "size": row.item.size,
+                "quantity": quantity,
+                "price": price
+              };
+              result.set(key, newValue);
+            }
           }
         } //end for
 
@@ -80,43 +82,46 @@ controller.getStock = function (req, res) {
 
         for (var i = 0; i < size; i++) {
           var row = data[i];
-          var key = row.item._id;
-          if (row.warehouse) {
-            key = row.warehouse._id + '|' + row.item._id;
-          }
-
-          if (result.has(key)) {
-            var value = result.get(key);
-            var type = row.type;
-            if ('I' == type) {
-              value.quantity = value.quantity + row.quantity;
-            } else {
-              value.quantity = value.quantity - row.quantity;
-            }
-            result.set(key, value);
-          } else {
-            var warehouse = '';
+          if (row.item && row.item._id && row.warehouse && row.warehouse._id) {
+            var key = row.item._id;
             if (row.warehouse) {
-              warehouse = row.warehouse.name;
-            }
-            var quantity = 0;
-            var type = row.type;
-            if ('I' == type) {
-              quantity = row.quantity;
-            } else {
-              quantity = -row.quantity;
+              key = row.warehouse._id + '|' + row.item._id;
             }
 
-            var newValue = {
-              "warehouse": warehouse,
-              "item": row.item.model,
-              "category": row.item.category,
-              "gender": row.item.gender,
-              "size": row.item.size,
-              "quantity": quantity
-            };
-            result.set(key, newValue);
+            if (result.has(key)) {
+              var value = result.get(key);
+              var type = row.type;
+              if ('I' == type) {
+                value.quantity = value.quantity + row.quantity;
+              } else {
+                value.quantity = value.quantity - row.quantity;
+              }
+              result.set(key, value);
+            } else {
+              var warehouse = '';
+              if (row.warehouse) {
+                warehouse = row.warehouse.name;
+              }
+              var quantity = 0;
+              var type = row.type;
+              if ('I' == type) {
+                quantity = row.quantity;
+              } else {
+                quantity = -row.quantity;
+              }
+
+              var newValue = {
+                "warehouse": warehouse,
+                "item": row.item.model,
+                "category": row.item.category,
+                "gender": row.item.gender,
+                "size": row.item.size,
+                "quantity": quantity
+              };
+              result.set(key, newValue);
+            }
           }
+
         } //end for
 
         var arr = [];
